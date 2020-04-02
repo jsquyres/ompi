71e77830b9a08494ae9ecb397b45a36119a3a927
Jeff Squyres <jsquyres@cisco.com>
2020-04-02 12:41:38 -0400

    squashme WIP
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

---
+++ ompi/tools/mpirun/mpirun.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+echo "heloo, this is mpirun.sh"
+
+# JMS to do:
+# 1. search for --net and --tuned; display warning/explanation/abort
+# 2. exec prte, but make sure quoting is correct and space-including-tokens aren't expanded
