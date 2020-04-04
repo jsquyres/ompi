bb6172259bf753afa40a2d3cb576b8d35c7815c3
Jeff Squyres <jsquyres@cisco.com>
2020-04-04 14:37:09 -0400

    WIP Added -tune functionality
    According to the prun.1 man page
    
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

--- ompi/tools/mpirun/mpirun.sh
+++ ompi/tools/mpirun/mpirun.sh
@@ -1,7 +1,27 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-echo "heloo, this is mpirun.sh"
+# Search to see if the unsupported options were used
+found=
+for token in $*; do
+    if test "$token" == "--net" -o "$token" == "--tuned"; then
+        found=$token
+    fi
+done
 
-# JMS to do:
-# 1. search for --net and --tuned; display warning/explanation/abort
-# 2. exec prte, but make sure quoting is correct and space-including-tokens aren't expanded
+if test -n "$found"; then
+    cat <<EOF
+-----------------------------------------------------------------------
+You used an $0 option that is not supported: $found
+
+This option is only supported on systems that have Python v3 or higher.
+To fix this issue, please re-install Open MPI with Python v3 (or higher)
+available in your PATH.
+-----------------------------------------------------------------------
+EOF
+    exit 1
+fi
+
+# No unsupported options were used, so pass all the options down to prte.
+# Note the Bash-specific syntax here: "$@" -- all 4 characters are important.
+# This preserves tokens that include spaces.  See bash(1) for details.
+exec prte "$@"
