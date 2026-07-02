ce9be2ac818a5520a65ed82836c6b83ad19ecb5c
Howard Pritchard <howardp@lanl.gov>
2026-07-02 08:20:28 -0600

    Address PR 13280 review findings (partial)
    - Add copyright header and include guard to ompi/util/status.h
    - Fix double semicolon in cvar_handle_alloc.c.in
    - Remove redundant OMPI_STANDARD_ABI conditional in Makefile_abi.include
    - Fix NULL handle validation bugs in cvar_write.c.in, pvar_read.c.in, and pvar_write.c.in
    
    The NULL handle bugs were pre-existing but are now fixed to prevent
    potential NULL pointer dereferences.
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- ompi/util/status.h
+++ ompi/util/status.h
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University and Indiana
+ *                         University Research and Technology
+ *                         Corporation.  All rights reserved.
+ * Copyright (c) 2004-2005 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
+ *                         University of Stuttgart.  All rights reserved.
+ * Copyright (c) 2004-2005 The Regents of the University of California.
+ *                         All rights reserved.
+ * Copyright (c) 2025      Triad National Security, LLC. All rights
+ *                         reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#ifndef OMPI_UTIL_STATUS_H
+#define OMPI_UTIL_STATUS_H
+
 #include "ompi/mpi/fortran/base/fint_2_int.h"
 #include "ompi/mpi/fortran/base/constants.h"
 
@@ -54,3 +77,5 @@ static inline int ompi_status_f2c(const MPI_Fint *f_status, MPI_Status *c_status
 
     return MPI_SUCCESS;
 }
+
+#endif /* OMPI_UTIL_STATUS_H */
